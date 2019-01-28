9427ca7021e6c3ff8fee737202efe3e556f40de7
Mike <peterm30@erau.edu>
2019-01-27 20:01:46 -0500

    Version 2.0

---
+++ EventFinder/Form1.Designer.cs
@@ -0,0 +1,149 @@
+﻿namespace EventFinder
+{
+    partial class FindEvents
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.StartButton = new System.Windows.Forms.Button();
+            this.EndButton = new System.Windows.Forms.Button();
+            this.StartInput = new System.Windows.Forms.TextBox();
+            this.EndInput = new System.Windows.Forms.TextBox();
+            this.FindEventsButton = new System.Windows.Forms.Button();
+            this.StatusOutput = new System.Windows.Forms.Label();
+            this.EventRangeBox = new System.Windows.Forms.GroupBox();
+            this.Status = new System.Windows.Forms.GroupBox();
+            this.EventRangeBox.SuspendLayout();
+            this.Status.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // StartButton
+            // 
+            this.StartButton.Location = new System.Drawing.Point(6, 37);
+            this.StartButton.Name = "StartButton";
+            this.StartButton.Size = new System.Drawing.Size(123, 52);
+            this.StartButton.TabIndex = 0;
+            this.StartButton.Text = "Start Time";
+            this.StartButton.UseVisualStyleBackColor = true;
+            this.StartButton.Click += new System.EventHandler(this.StartButton_Click);
+            // 
+            // EndButton
+            // 
+            this.EndButton.Location = new System.Drawing.Point(6, 95);
+            this.EndButton.Name = "EndButton";
+            this.EndButton.Size = new System.Drawing.Size(123, 53);
+            this.EndButton.TabIndex = 1;
+            this.EndButton.Text = "End Time";
+            this.EndButton.UseVisualStyleBackColor = true;
+            this.EndButton.Click += new System.EventHandler(this.EndButton_Click);
+            // 
+            // StartInput
+            // 
+            this.StartInput.Location = new System.Drawing.Point(153, 48);
+            this.StartInput.Name = "StartInput";
+            this.StartInput.Size = new System.Drawing.Size(256, 29);
+            this.StartInput.TabIndex = 2;
+            // 
+            // EndInput
+            // 
+            this.EndInput.Location = new System.Drawing.Point(153, 106);
+            this.EndInput.Name = "EndInput";
+            this.EndInput.Size = new System.Drawing.Size(256, 29);
+            this.EndInput.TabIndex = 3;
+            // 
+            // FindEventsButton
+            // 
+            this.FindEventsButton.Location = new System.Drawing.Point(142, 198);
+            this.FindEventsButton.Name = "FindEventsButton";
+            this.FindEventsButton.Size = new System.Drawing.Size(147, 51);
+            this.FindEventsButton.TabIndex = 4;
+            this.FindEventsButton.Text = "Find Events";
+            this.FindEventsButton.UseVisualStyleBackColor = true;
+            this.FindEventsButton.Click += new System.EventHandler(this.FindEventsButton_Click);
+            // 
+            // StatusOutput
+            // 
+            this.StatusOutput.AutoSize = true;
+            this.StatusOutput.Location = new System.Drawing.Point(16, 35);
+            this.StatusOutput.Name = "StatusOutput";
+            this.StatusOutput.Text = "Checking for administrator rights...";
+            this.StatusOutput.Size = new System.Drawing.Size(0, 25);
+            this.StatusOutput.TabIndex = 5;
+            // 
+            // EventRangeBox
+            // 
+            this.EventRangeBox.Controls.Add(this.StartButton);
+            this.EventRangeBox.Controls.Add(this.EndButton);
+            this.EventRangeBox.Controls.Add(this.StartInput);
+            this.EventRangeBox.Controls.Add(this.EndInput);
+            this.EventRangeBox.Location = new System.Drawing.Point(12, 13);
+            this.EventRangeBox.Name = "EventRangeBox";
+            this.EventRangeBox.Size = new System.Drawing.Size(423, 168);
+            this.EventRangeBox.TabIndex = 6;
+            this.EventRangeBox.TabStop = false;
+            this.EventRangeBox.Text = "Event Range";
+            // 
+            // Status
+            // 
+            this.Status.Controls.Add(this.StatusOutput);
+            this.Status.Location = new System.Drawing.Point(18, 268);
+            this.Status.Name = "Status";
+            this.Status.Size = new System.Drawing.Size(403, 147);
+            this.Status.TabIndex = 7;
+            this.Status.TabStop = false;
+            this.Status.Text = "Status";
+            // 
+            // FindEvents
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(447, 427);
+            this.Controls.Add(this.FindEventsButton);
+            this.Controls.Add(this.EventRangeBox);
+            this.Controls.Add(this.Status);
+            this.Name = "FindEvents";
+            this.Text = "FindEvents";
+            this.EventRangeBox.ResumeLayout(false);
+            this.EventRangeBox.PerformLayout();
+            this.Status.ResumeLayout(false);
+            this.Status.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button StartButton;
+        private System.Windows.Forms.Button EndButton;
+        private System.Windows.Forms.TextBox StartInput;
+        private System.Windows.Forms.TextBox EndInput;
+        private System.Windows.Forms.Button FindEventsButton;
+        private System.Windows.Forms.Label StatusOutput;
+        private System.Windows.Forms.GroupBox EventRangeBox;
+        private System.Windows.Forms.GroupBox Status;
+    }
+}
+
---
+++ EventFinder/Form1.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics.Eventing.Reader; // I think this will be needed?
+using System.Diagnostics;
+using System.Xml;
+using System.Xml.Linq;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+using System.IO;
+using CsvHelper;
+
+public class Record
+{
+    public string Message { get; set; }
+    public string SystemTime { get; set; }
+    public string Id { get; set; }
+    public string Version { get; set; }
+    public string Qualifiers { get; set; }
+    public string Level { get; set; }
+    public string Task { get; set; }
+    public string Opcode { get; set; }
+    public string Keywords { get; set; }
+    public string RecordId { get; set; }
+    public string ProviderName { get; set; }
+    public string ProviderID { get; set; }
+    public string LogName { get; set; }
+    public string ProcessId { get; set; }
+    public string ThreadId { get; set; }
+    public string MachineName { get; set; }
+    public string UserID { get; set; }
+    public string TimeCreated { get; set; }
+    public string ActivityId { get; set; }
+    public string RelatedActivityId { get; set; }
+    public string Hashcode { get; set; }
+    public string MatchedQueryIds { get; set; }
+    public string LevelDisplayName { get; set; }
+    public string OpcodeDisplayName { get; set; }
+    public string TaskDisplayName { get; set; }
+
+}
+
+
+namespace EventFinder
+{
+    public partial class FindEvents : Form
+    {
+        public FindEvents()
+        {
+            InitializeComponent();
+
+
+
+        EventLogSession session = new EventLogSession();
+            var providers = session.GetProviderNames().ToList();
+            Regex rgx = new Regex(@"^Security$");
+            bool AdminFlag = false;
+
+            foreach (string provider in providers)
+            {
+                if (rgx.IsMatch(provider))
+                {
+                    AdminFlag = true;
+                }
+            }
+
+            if (AdminFlag)
+            {
+                StatusOutput.Text = "Security log found!\nYou are likely administrator";
+                StatusOutput.ForeColor = System.Drawing.Color.Green;
+            } else
+            {
+                StatusOutput.Text = "Unable to read Security log.\nAre you administrator?";
+                StatusOutput.ForeColor = System.Drawing.Color.Red;
+            }
+        }
+
+        private void StartButton_Click(object sender, EventArgs e)
+        {
+            string CurrentTime = DateTime.Now.ToString("MM/dd/yyyy HH:mm:ss");
+            StartInput.Text = CurrentTime;
+        }
+
+        private void EndButton_Click(object sender, EventArgs e)
+        {
+            string CurrentTime = DateTime.Now.ToString("MM/dd/yyyy HH:mm:ss");
+            EndInput.Text = CurrentTime;
+        }
+
+        private void FindEventsButton_Click(object sender, EventArgs e)
+        {
+            FindEventsButton.Enabled = false;
+
+
+
+            
+
+        // Variables we will need
+        DateTime StartTime = DateTime.ParseExact(StartInput.Text, "MM/dd/yyyy HH:mm:ss", null);
+        DateTime EndTime = DateTime.ParseExact(EndInput.Text, "MM/dd/yyyy HH:mm:ss", null);
+        string RunTime = DateTime.Now.ToString("yyyyMMdd_HHmmss");
+        string DesktopPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+        EventLogSession Session = new EventLogSession();
+        var Providers = Session.GetProviderNames().ToList();
+
+        var query = string.Format(@"*[System[TimeCreated[@SystemTime >= '{0}']]] and *[System[TimeCreated[@SystemTime <= '{1}']]]", StartTime.ToString("o"), EndTime.ToString("o"));
+
+            List<Record> records = new List<Record> { };
+
+            foreach (var Provider in Providers)
+            {
+                try
+                {
+                    EventLogQuery eventlogQuery = new EventLogQuery(Provider, PathType.LogName, query);
+                    EventLogReader eventlogReader = new EventLogReader(eventlogQuery);
+
+                    for (EventRecord eventRecord = eventlogReader.ReadEvent(); null != eventRecord; eventRecord = eventlogReader.ReadEvent())
+                    {
+                        // Get the SystemTime from the event record XML 
+                        var xml = XDocument.Parse(eventRecord.ToXml());
+                        XNamespace ns = xml.Root.GetDefaultNamespace();
+
+                        // Collect ALL THE THINGS!
+                        string Message = eventRecord.FormatDescription();
+                        string SystemTime = xml.Root.Element(ns + "System").Element(ns + "TimeCreated").Attribute("SystemTime").Value;
+                        string Id = eventRecord.Id.ToString();
+                        string Version = eventRecord.Version.ToString();
+                        string Qualifiers = eventRecord.Qualifiers.ToString();
+                        string Level = eventRecord.Level.ToString();
+                        string Task = eventRecord.Task.ToString();
+                        string Opcode = eventRecord.Opcode.ToString();
+                        string Keywords = eventRecord.Keywords.ToString();
+                        string RecordId = eventRecord.RecordId.ToString();
+                        string ProviderName = eventRecord.ProviderName;
+                        string ProviderID = eventRecord.ProviderId.ToString();
+                        string LogName = eventRecord.LogName;
+                        string ProcessId = eventRecord.ProcessId.ToString();
+                        string ThreadId = eventRecord.ThreadId.ToString();
+                        string MachineName = eventRecord.MachineName;
+                        string UserID = eventRecord.UserId?.ToString();
+                        string TimeCreated = eventRecord.TimeCreated.ToString();
+                        string ActivityId = eventRecord.ActivityId.ToString();
+                        string RelatedActivityId = eventRecord.RelatedActivityId.ToString();
+                        string Hashcode = eventRecord.GetHashCode().ToString();
+                        string LevelDisplayName = eventRecord.LevelDisplayName;
+                        string OpcodeDisplayName = eventRecord.OpcodeDisplayName;
+                        string TaskDisplayName = eventRecord.TaskDisplayName;
+
+                        // Add them to the record. The things equal the things.
+                        records.Add(new Record() { Message = Message, SystemTime = SystemTime, Id = Id, Version = Version, Qualifiers = Qualifiers, Level = Level, Task = Task, Opcode = Opcode, Keywords = Keywords, RecordId = RecordId, ProviderName = ProviderName, ProviderID = ProviderID, LogName = LogName, ProcessId = ProcessId, ThreadId = ThreadId, MachineName = MachineName, UserID = UserID, TimeCreated = TimeCreated, ActivityId = ActivityId, RelatedActivityId = RelatedActivityId, Hashcode = Hashcode, LevelDisplayName = LevelDisplayName, OpcodeDisplayName = OpcodeDisplayName, TaskDisplayName = TaskDisplayName });
+                    }
+
+                } catch (EventLogNotFoundException)
+                {
+                    // No events found - Nothing to be done
+                } catch (EventLogException)
+                {
+                    // Error Reading Provider - Nothing to be done
+                }
+
+
+            }
+
+            records.OrderBy(x => x.SystemTime);
+
+            using (var writer = new StreamWriter(DesktopPath + "\\Logs_Runtime_" + RunTime + ".csv", append: true))
+            using (var csv = new CsvWriter(writer))
+            {
+                csv.Configuration.ShouldQuote = (field, context) => true;
+                csv.WriteRecords(records);
+            }
+
+            FindEventsButton.Enabled = true;
+        }
+    }
+}
